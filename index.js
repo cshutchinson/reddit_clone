cad14d1ca5029b1223d3f67e7852994c704bd958
Christopher Hutchinson <cshutchinson@gmail.com>
2016-01-21 16:39:42 -0700

    new post creation in progress - no validation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['angularMoment']);
 // image title upvotes, descirption date comments author
 var posts = [{
   id: 10,
-  date: '01-05-2016',
+  date: new Date('01/20/2016'),
   author: 'Christopher Hutchinson',
   title: 'Panoramic Photo Gone Wrong',
   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
@@ -24,4 +24,5 @@ var posts = [{
 
 app.controller("redditController", function($scope){
   $scope.posts = posts;
+  // $scope.newpost = false;
 })
